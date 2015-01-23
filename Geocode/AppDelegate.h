1e8cdfa0c17ec69031e7fa16257bfbe05038125d
Marius Horga <mhorga@gmail.com>
2015-01-23 13:26:07 +0200

    Initial Commit

---
+++ Geocode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Geocode
+//
+//  Created by Marius Horga on 1/23/15.
+//  Copyright (c) 2015 Marius Horga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Geocode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Geocode
+//
+//  Created by Marius Horga on 1/23/15.
+//  Copyright (c) 2015 Marius Horga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
